7d4895ce45ccb5dd66717fdc2243340361536675
Michael Brown <micha3lbrown@gmail.com>
2017-10-30 23:44:38 -0400

    FeedParser parsing FreeBSD vuln feed
    - Basic feed parsing implemented using FreeBSD vulnerability XML feed
    - Adjusting to NodeJS streams and an Event based system
    - README.md right now contains notes about useful resources, this will
    be gradually phased out as the project becomes more mature.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # grigori
 Vulnerability feed parser
+
+## Notes
+- [Feed Parser](https://github.com/danmactough/node-feedparser#what-is-the-parsed-output-produced-by-feedparser) is what I'm using to parse XML feeds.
+
+Node 101
+`Events`: Much of the Node.js core API is built around an idiomatic asynchronous event-driven architecture in which certain kinds of objects (called "emitters") periodically emit named events that cause Function objects ("listeners") to be called.
+|
+|--> EventEmitter --> Stream
+
+`Stream`: A stream is an abstract interface for working with streaming data in Node.js. The stream module provides a base API that makes it easy to build objects that implement the stream interface. [More on streams here](https://www.sitepoint.com/basics-node-js-streams/)
+
+## TODO
+- [x] get XML feedparsing working and inspect objects
+- [ ] investigate other feeds to find similarities
+- [ ] scaffold a schema
+- [ ] storage options, probably SQL based
---
+++ main.js
@@ -0,0 +1,40 @@
+// Required libs
+var FeedParser = require('feedparser'),
+    request = require('request');
+
+// Initialization
+var req = request('http://vuxml.freebsd.org/freebsd/rss.xml'),
+    feedparser = new FeedParser();
+
+// Request stream handling
+req.on('error', function (error) {
+  this.emit('error', new Error('Something went wrong with the instantiating the `request`');
+})
+
+req.on('response', function (res) {
+  var stream = this; // `this` == req which is a stream with an EventEmitter listening for response?
+
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Expected status code 200, but received...'));
+  }
+
+  else {
+    this.pipe(feedparser);
+  }
+
+});
+
+// FeedParser stream handling
+feedparser.on('error', function (error) {
+  this.emit('error', new Error('Something went wrong with the feed parser, oh no!'));
+});
+
+feedparser.on('readable', function() {
+  var meta = this.meta,
+      item;
+
+  while (item = this.read()) {
+    console.log(item);
+  }
+
+});
